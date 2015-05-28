0f54de8dd003a005352e5fdb4219bb0a1767e5ba
rafareusch <rafa_reusch@hotmail.com>
2015-05-28 18:17:21 -0300

    GG

--- rafael.c
+++ rafael.c
@@ -21,7 +21,8 @@ struct celula {
 //Essa função será executada na main, para que o jogador realize suas jogadas, até o final do jogo
 // Ela recebe por parâmetro a referência para o tabuleiro, juntamente com o tamanho do lado do tabuleiro e o número total de bombas
 // A função deve retornar 1 enquanto o jogo não acabar e 0 quando ele acabar
-int jogada(struct celula *tabuleiro, int lado, int total_bombas);
+int jogada(struct celula *tabuleiro, int lado, int total_bombas){
+}
 
 
 //Essa função facilita funções de abertura de célula e de cálculo de valor de células
@@ -39,8 +40,7 @@ int le_vizinho(int x, int y, int posicao, int controle, struct celula *tabuleiro
 
 //Essa função é responsável por criar bombas no tabuleiro de forma aleatória (usando uma semente para inicialização da função rand()). Além disso, essa função deve calcular os valores de controle de cada célula do tabuleiro (conforme definido na específicação desse trabalho).
 // Ela recebe por parâmetro a referência para o tabuleiro, juntamente com o tamanho do lado do tabuleiro, o número total de bombas e a semente a ser usada na geração das mesmas
-int gera_bombas(struct celula *tabuleiro, int lado, int num_bombas, int semente)
-{
+int gera_bombas(struct celula *tabuleiro, int lado, int num_bombas, int semente){
 	int x, y, bomb=0;
 	while(bomb != num_bombas){	
 		x = rand()%(lado-1);
@@ -59,9 +59,35 @@ int gera_bombas(struct celula *tabuleiro, int lado, int num_bombas, int semente)
 // Ela recebe por parâmetro a referência para o tabuleiro e o tamanho do lado do tabuleiro
 int imprime_tabuleiro(struct celula *tabuleiro, int lado){
  	int i,n;
+ 	
+ 	for (i=0;i<lado;i=i+2){
+		if (i==0)
+			printf("\n   %d",i);
+		else if (i<10)
+ 			printf("     %d", i);
+ 			else
+ 			printf("    %d", i);
+ 	}
+ 	printf("\n");
+ 	for (i=1;i<lado;i=i+2)
+ 	if (i==1)
+			printf("      %d",i);
+		else if (i<10)
+ 			printf("     %d", i);
+ 			else
+ 			printf("    %d", i);
+ 	
+ 		
+ 		
+ 	printf("\n");
  	for (i=0;i<lado;i++){
-	 	for (n=0; n<lado;n++)
-				 printf(" %c ", tabuleiro[i*lado + n].imprime);
+ 			if (i<10)
+ 				printf(" ");
+ 		printf("%d", i);
+	 	for (n=0; n<lado;n++){
+	 			
+				printf(" %c ", tabuleiro[i*lado + n].imprime);
+		}
  	printf("\n");
 	}		
 }
@@ -72,12 +98,24 @@ int imprime_tabuleiro(struct celula *tabuleiro, int lado){
 // Caso a célula não esteja próxima a nenhuma bomba (valor=0), a função deve seguir expandindo todas células vizinhas, até chegar nas margens de bombas próximas (células com valor>0)
 // Se o usuário abrir uma célula que é uma bomba, ele perde o jogo
 // Células marcadas como bombas não podem ser abertas e deverão primeiramente ser desmarcadas, para então serem abertas
-int abrir_celula(struct celula **tabuleiro, int lado);
+int abrir_celula(struct celula *tabuleiro, int lado);
 
 
 //Essa função solicita uma coordenada XY (usando as funções printf() e scanf()) e marca uma célula no tabuleiro como bomba. Caso a célula já esteja marcada como bomba, a função desmarca a célula
 // Ela recebe por parâmetro a referência para o tabuleiro e o tamanho do lado do tabuleiro
-int marcar_bomba(struct celula *tabuleiro, int lado);
+int marcar_bomba(struct celula *tabuleiro, int lado){
+	int x,y;
+	printf("Entre com a posicao desejada\n");
+	printf("X=");
+	scanf("%d",&x);
+	printf("Y=");
+	scanf("%d",&y);
+	if (tabuleiro[x*lado + y].imprime == 'B'){
+		tabuleiro[x*lado + y].imprime = 35;
+	}
+	else
+	tabuleiro[x*lado + y].imprime = 66;
+}
 
 
 //Essa função é executada no final do jogo. Ela escreve a pontuação do jogador no arquivo texto pontos.txt.
@@ -89,34 +127,57 @@ int marcar_bomba(struct celula *tabuleiro, int lado);
 // Antes de finalizar o jogo, essa função deve imprimir os 10 melhores jogadores
 int fim_de_jogo(char jogador[20], int lado, int num_bombas, int celulas_abertas);
 
+int opcoes(){
+	printf("\n	COMANDOS:\n");
+	printf(" a - Abrir posicao no tabuleiro\n b - Marcar/Desmarcar posicao no tabuleiro como bomba\n");
+	printf(" p - Lista pontuacoes\n c - Lista de comandos\n\n");
+}
 
-
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]){
 	//Tabuleiro e variáveis para controle
 	struct celula *tabuleiro, *tabuleiro_aux;
 	int lado_tabuleiro=atoi(argv[2]), num_bombas=atoi(argv[3]), semente;
 	//Nome do jogador
-	char jogador[20];
+	char jogador[20],o;
 	//Para controle de laços
 	int i,n;
-
+	
 	//Escreva aqui um trecho de código para verificar a consistência de dados de entrada do programa (argc e argv)
-	if (argc!=4 	||	 lado_tabuleiro < 2 	|| 	lado_tabuleiro*lado_tabuleiro < num_bombas){
+	if (argc!=4 || lado_tabuleiro < 2 || lado_tabuleiro*lado_tabuleiro <= num_bombas){
 		printf("Erro");
 		return 0;
 	}
-	
+	printf("\nJogador %s, Tabuleiro %dx%d com %d Bombas!\n", argv[1],lado_tabuleiro,lado_tabuleiro,num_bombas);
 	//Escreva aqui um trecho de código para inicializar as variáveis do jogo
 	
 	//Escreva aqui um trecho de código para inicialização do tabuleiro e alocação de espaço em memória (usar malloc)
+	
 	tabuleiro = (struct celula*)malloc(sizeof(struct celula)*lado_tabuleiro*lado_tabuleiro);
-
 	for (i=0;i<lado_tabuleiro;i++)
 		for (n=0; n<lado_tabuleiro;n++)
  			 tabuleiro[i*lado_tabuleiro + n].imprime = 35;
  			 
-	imprime_tabuleiro(tabuleiro,lado_tabuleiro);				 
+ 	opcoes();	
+	while(1){
+		imprime_tabuleiro(tabuleiro,lado_tabuleiro);
+		printf("Insira a opcao (a/b/c/s): ");
+		scanf(" %c", &o);
+		switch(o){
+			case 'a':
+				jogada(tabuleiro,lado_tabuleiro,num_bombas);
+				 break;
+			case 'b':
+				marcar_bomba(tabuleiro,lado_tabuleiro);
+				break;
+			case 'c':
+				opcoes();
+				break;
+			case 's':
+				return 0;
+		}
+	}
+
+	
 	//Aqui as bombas devem ser criadas no tabuleiro e os valores de controle de todas células devem ser calculados
 	srand(time(NULL));
 	semente=rand()%666;
@@ -125,6 +186,12 @@ int main (int argc, char *argv[])
 	//Escreva aqui um trecho de código para controle de rodadas do jogo
 	
 	//Escreva aqui um trecho de código para finalizar o jogo
-
+	
 	return 0;
 }
+/* IMRPIME TABULEIRO VALORES
+for (i=0;i<lado_tabuleiro;i++){
+	for (n=0; n<lado_tabuleiro;n++)
+	 		 printf(" %d ", tabuleiro[i*lado_tabuleiro + n].valor);
+ 	printf(" \n ");
+	}*/
